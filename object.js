337d8006395de1c0bf77948c3982f70771842232
louisse2021 <100004777+louisse2021@users.noreply.github.com>
2023-04-15 19:12:37 -0400

    home description

---
+++ object.js
@@ -0,0 +1,11 @@
+const myHouse = {
+    style: 'Victorian',
+    color: 'White',
+    isItHighCeillings: 'True',
+    ringBellCameras: 2,
+    appsForCameras: ['Webcam'],
+    proofOfObjectInObject: {}
+}
+
+console.log('myHouse', myHouse)
+console.log('my apps, myHouse.appsForCameras')
